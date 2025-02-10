c8da87d6fb3d2596b4597e8bff1bb1bf6092e318
Shepard2154 <john-shepard-2154@yandex.ru>
2025-02-10 17:44:45 +0300

    refactor[program #1]: added OOP style to code

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ A small application for analyzing IPv4 (TCP + UDP) packets in pcap files
 
 # Compilation stage
 1. Open terminal from directory with "main.cpp"
-2. Compile the code from the directory containing "main.cpp" with command "g++ -L ../../libs/libpcap-1.10.5 main.cpp -o main -lpcap"
+2. Compile the code from the directory containing "g++ -L ../../libs/libpcap-1.10.5 main.cpp sniffer.cpp -o main -lpcap"
--- src/c++/main.cpp
+++ src/c++/main.cpp
@@ -3,82 +3,16 @@
 using namespace std;
 
 
-void print_ip_packet_info(const sniff_ip* packet, pcap_pkthdr* header) {
-    cout << "packet IP version (class): " << packet -> ip_vhl << endl;
-    cout << "packet IP protocol: " << int(packet -> ip_p) << endl;
-
-    in_addr_t source_address = packet -> ip_src.s_addr;
-    char source_string[IP_ADDRESS_SIZE];
-    cout << "packet IP source address: " << inet_ntop(AF_INET, &source_address, source_string, sizeof(source_string)) << endl;
-
-    in_addr_t destination_address = packet -> ip_dst.s_addr;
-    char destination_string[16];
-    cout << "packet IP destination address: " << inet_ntop(AF_INET, &destination_address, destination_string, sizeof(destination_string)) << endl;
-
-    cout << "Total length: " << ntohs(packet -> ip_len) << endl;
-    cout << "Packet size: " << header -> len << endl;
-    cout << "Number of bytes: " << header -> caplen << endl;
-    cout << endl;
-}
-
-void print_tcp_header(const struct sniff_tcp *tcp) {
-    cout << "TCP source port: " << ntohs(tcp -> th_sport) << endl;
-    cout << "TCP destination port: " << ntohs(tcp -> th_dport) << endl;
-}
-
-void print_udp_header(const struct sniff_udp *udp) {
-    cout << "UDP source port: " << to_string(ntohs(udp -> uh_sport)) << endl;
-    cout << "UDP destination port: " << to_string(ntohs(udp-> uh_dport)) << endl;
-    cout << "UDP length: " << to_string(ntohs(udp -> uh_len)) << endl;
-    cout << "UDP checksum: " << to_string(ntohs(udp -> uh_sum)) << endl << endl;
-}
-
-void write_to_csv(const string& filename, const map<tuple<string, string, string, string>, vector<int>>& data) {
-    int packets_count = 0;
-    int packets_bytes_size = 0;
-
-    ofstream file(filename);
-    if (!file.is_open()) {
-        std::cerr << "Ошибка открытия файла!" << std::endl;
-        return;
-    }
-
-    // Записываем заголовок CSV
-    file << "IP_source,IP_destination,port_source,port_destination,packets_count,packets_bytes\n";
-
-    // Записываем данные
-    for (const auto& entry : data) {
-        const auto& keys = entry.first; // Пара ключей
-        const auto& values = entry.second; // Вектор значений
-
-        // Записываем ключи
-        file << get<0>(keys) << "," << get<1>(keys) << "," << get<2>(keys) << ',' << get<3>(keys);
-
-        // Записываем значения
-        for (int value : values) {
-            packets_count++;
-            packets_bytes_size += value;
-        }
-
-        // Переход на новую строку
-        file << "," << packets_count << "," << packets_bytes_size << "\n";
-    }
-
-    // Закрываем файл
-    file.close();
-}
-
-
-int main(int argc, char *argv[]) {
-    string file_path = "../assets/example-02.pcap";
+int main(int argc, char *argv[]) {   
     char error_buffer[PCAP_ERRBUF_SIZE];
     int pcap_init(unsigned int opts, char *error_buffer);
+
     char *dev, errbuf[PCAP_ERRBUF_SIZE];
     pcap_if_t **alldevsp;
-    bpf_u_int32 mask,net;
+    bpf_u_int32 mask, net;
     pcap_lookupnet(dev, &net, &mask, errbuf);
     
-    // UNCOMMENT TO CATCH PACKETS FROM NET INTERFACE 
+    // TODO - begin: CAPTURING PACKETS FROM NET INTERFACE 
     // dev = pcap_lookupdev(errbuf);
     // if (dev == NULL) 
     // {
@@ -87,7 +21,9 @@ int main(int argc, char *argv[]) {
     // }
     // printf("Device: %s\n", dev);
     // return(0);
+    // TODO - end;
 
+    string file_path = "../assets/example-02.pcap";
     pcap_t *handle = pcap_open_offline(file_path.c_str(), error_buffer);
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open file %s: %s\n", file_path.c_str(), error_buffer);
@@ -131,7 +67,8 @@ int main(int argc, char *argv[]) {
             continue;
         }
         
-        print_ip_packet_info(ip, header);
+        IpExtractor ip_extractor(*ip);
+        ip_extractor.PrintInfo(header);
 
         size_ip = IP_HL(ip) * 4;
         if (size_ip < 20 && size_ip > 60) {
@@ -152,13 +89,17 @@ int main(int argc, char *argv[]) {
                 return -1;
             }
 
-            print_tcp_header(tcp);
+            TcpExtractor tcp_extractor(*tcp);
+            tcp_extractor.PrintInfo();
 
             source_port = to_string(ntohs(tcp -> th_sport));
             destination_port = to_string(ntohs(tcp -> th_dport));
         } else if (int(ip -> ip_p) == 17) {
             udp = (struct sniff_udp*)(packet + SIZE_ETHERNET + size_ip);
-            print_udp_header(udp);
+            
+            UdpExtractor udp_extractor(*udp);
+            udp_extractor.PrintInfo();
+
             source_port = to_string(ntohs(udp -> uh_sport));
             destination_port = to_string(ntohs(udp -> uh_dport));
         } else {
